8aab3f813e103ab970988777a11b94bfb2bcdd27
Allan BARRIELLE <abarriel@e1r6p6.42.fr>
2017-03-22 22:56:50 +0100

    add label

--- asm/srcs/label.c
+++ asm/srcs/label.c
@@ -6,10 +6,16 @@
 /*   By: abarriel <abarriel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/22 22:19:11 by abarriel          #+#    #+#             */
-/*   Updated: 2017/03/22 22:23:05 by abarriel         ###   ########.fr       */
+/*   Updated: 2017/03/22 22:56:28 by abarriel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
+void	get_label(t_asm *a, t_header *h)
+{
+	char	*line;
 
+	while (get_next_line(a->fd_cor, &line))
+		ft_printf("{8},%s\n",line);
+}
