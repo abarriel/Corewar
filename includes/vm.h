765e6d93227b76ed054e4f281bb32141ef34cec7
Lucas CHARVOLIN <lcharvol@e3r1p2.42.fr>
2017-03-26 11:06:51 +0200

    actu vm

--- includes/vm.h
+++ includes/vm.h
@@ -14,7 +14,12 @@
 # define VM_H
 # include "libft.h"
 # include "op.h"
-
+# include "mlx/mlx.h"
+# define WIDTH 2000
+# define HEIGHT 1250
+# define ECHAP  53
+# define MAP_SIZE_X 64
+# define MAP_SIZE_Y 64
 # define CYC_SEC_ST 50
 
 typedef struct			s_process
@@ -43,6 +48,7 @@ typedef struct			s_core
 {
   t_process   *process;
   t_player    *player;
+  int         visu;
   int	        nb_player;
   char        mem[MEM_SIZE];
   char        mem_c[MEM_SIZE];
@@ -52,6 +58,51 @@ typedef struct			s_core
   char        run;
 }						t_core;
 
+typedef struct    s_env
+{
+  int       speed;
+  int       pause;
+  int       cycle_count;
+  struct s_but  *but;
+  int       h;
+  int       l;
+  int       r;
+  int       v;
+  int       b;
+  int       map_size_x;
+  int       map_size_y;
+  char      *ret;
+  void      *ret2;
+  void      *win;
+  void      *mlx;
+  void      *img;
+  void      *img2;
+  int       bits_per_pixel;
+  int       size_line;
+  int       endian;
+}         t_env;
+
+typedef struct    s_but
+{
+  int       btn1_s;
+  int       btn2_s;
+  int       btn3_s;
+  int       btn4_s;
+  int       btn5_s;
+  int       btn1_px;
+  int       btn2_px;
+  int       btn3_px;
+  int       btn4_px;
+  int       btn5_px;
+  int       btn1_py;
+  int       btn2_py;
+  int       btn3_py;
+  int       btn4_py;
+  int       btn5_py;
+}         t_but;
+
+
+int           visu(t_core *c);
 void           init_player(t_player *p);
 void					error_executable(void);
 t_core					*parcing(int argc, char **argv, t_core *c);
@@ -59,5 +110,19 @@ t_core					*new_core();
 void					init_core(t_core *core);
 t_core					*parcing(int argc, char **argv, t_core *c);
 
+//visu
+
+void        ft_draw_rectangle(int start_x, int start_y, t_env *p);
+void        ft_draw_score(t_env *p);
+void        ft_draw_menu(t_env *p);
+void        ft_draw_arena(t_env *p);
+void        print_text(t_env *p);
+void        ft_modif_color(int r, int v, int b, t_env *p);
+void        ft_draw(t_env *p);
+void        ft_modif_color(int r, int v, int b, t_env *p);
+void        ft_draw_square(int start_x, int start_y,
+  int size, t_env *p);
+void        ft_draw_background(t_env *p);
+void        ft_draw_map(t_env *p);
 
 #endif
