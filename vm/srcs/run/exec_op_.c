93e98e796251e9f3a5821bc7cc362ed21669bd4f
Corentin SECCIA <cseccia@e2r4p7.42.fr>
2017-03-29 08:41:32 +0200

    early cmd

--- vm/srcs/run/exec_op_.c
+++ vm/srcs/run/exec_op_.c
@@ -6,11 +6,21 @@
 /*   By: abarriel <abarriel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/28 06:40:45 by abarriel          #+#    #+#             */
-/*   Updated: 2017/03/29 07:10:04 by cseccia          ###   ########.fr       */
+/*   Updated: 2017/03/29 08:40:31 by cseccia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
+
+unsigned int chatohi()
+{
+  unsigned int final;
+
+	final = 0;
+  final |= ((unsigned int)(str)[0] << 8);
+	final |= ((unsigned int)(str)[1]);
+  return (final);
+}
 
 unsigned int chatoi(unsigned char *str)
 {
@@ -32,28 +42,6 @@ void insert_in_reg(unsigned char *reg, unsigned int cpy)
   reg[3] = (cpy & 0x000000FF);
 }
 
-unsigned char aply_mask(unsigned char cde, int arg)
-{
-  if (arg == 1)
-    return ((cde & 192) >> 6);
-  if (arg == 2)
-    return ((cde & 48) >> 4);
-  if (arg == 3)
-    return ((cde & 12) >> 2);
-  return (cde & 3);
-}
-
-unsigned int get_n_arg(t_core *core, t_process *process, int arg)
-{
-  unsigned int res;
-  unsigned char cde;
-  int i;
-
-  cde = core->mem[(process->pc + 1) % MEM_SIZE];
-  cde = aply_mask(cde, arg);
-  return (res);
-}
-
 int exec_and(void *core, void *pro)
 {
   t_process *pr;
@@ -62,13 +50,114 @@ int exec_and(void *core, void *pro)
 
   cr = (t_core*)core;
   pr = (t_process*)pro;
-  res = get_n_arg(cr, pr, 1) & get_n_arg(cr, pr, 2);
+  res = get_n_arg(cr, pr, 1, 1) & get_n_arg(cr, pr, 2, 1);
   if (res == 0)
     pr->carry = 1;
   else
     pr->carry = 0;
   insert_in_reg(get_n_reg(cr, pr, 3), res);
   return (size_arg(cr, pr));
+}
+
+int exec_or(void *core, void *pro)
+{
+  t_process *pr;
+  t_core *cr;
+  unsigned int res;
+
+  cr = (t_core*)core;
+  pr = (t_process*)pro;
+  res = get_n_arg(cr, pr, 1, 1) | get_n_arg(cr, pr, 2, 1);
+  if (res == 0)
+    pr->carry = 1;
+  else
+    pr->carry = 0;
+  insert_in_reg(get_n_reg(cr, pr, 3), res);
+  return (size_arg(cr, pr));
+}
+
+int exec_xor(void *core, void *pro)
+{
+  t_process *pr;
+  t_core *cr;
+  unsigned int res;
+
+  cr = (t_core*)core;
+  pr = (t_process*)pro;
+  res = get_n_arg(cr, pr, 1, 1) ^ get_n_arg(cr, pr, 2, 1);
+  if (res == 0)
+    pr->carry = 1;
+  else
+    pr->carry = 0;
+  insert_in_reg(get_n_reg(cr, pr, 3), res);
+  return (size_arg(cr, pr));
+}
+
+int exec_ld(void *core, void *pro)
+{
+  t_process *pr;
+  t_core *cr;
+  unsigned int res;
+
+  res = chatoi(cr->mem[get_n_arg(cr, pr, 1, 1)]);
+  if (get_n_arg(cr, pr, 1, 1) == 0)
+    pr->carry = 1;
+  else
+    pr->carry = 0;
+  insert_in_reg(get_n_reg(cr, pr, 2), res);
+  return (size_arg(cr, pr))
+}
+
+int exec_lld(void *core, void *pro)
+{
+  t_process *pr;
+  t_core *cr;
+  unsigned int res;
+
+  res = chatoi(cr->mem[get_n_arg(cr, pr, 1, 0)]);
+  if (get_n_arg(cr, pr, 1, 1) == 0)
+    pr->carry = 1;
+  else
+    pr->carry = 0;
+  insert_in_reg(get_n_reg(cr, pr, 2), res);
+  return (size_arg(cr, pr))
+}
+
+int exec_ldi(void *core, void *pro)
+{
+  t_process *pr;
+  t_core *cr;
+  unsigned int res;
+
+  res = chatoi(cr->mem[get_n_arg(cr, pr, 1, 1) + get_n_arg(cr, pr, 2, 1)]);
+  if (get_n_arg(cr, pr, 1, 1) + get_n_arg(cr, pr, 2, 1) == 0)
+    pr->carry = 1;
+  else
+    pr->carry = 0;
+  insert_in_reg(get_n_reg(cr, pr, 3), res);
+  return (size_arg(cr, pr))
+}
+
+int exec_ldi(void *core, void *pro)
+{
+  t_process *pr;
+  t_core *cr;
+  unsigned int res;
+
+  res = chatoi(cr->mem[get_n_arg(cr, pr, 1, 0) + get_n_arg(cr, pr, 2, 0)]);
+  if (get_n_arg(cr, pr, 1, 1) + get_n_arg(cr, pr, 2, 1) == 0)
+    pr->carry = 1;
+  else
+    pr->carry = 0;
+  insert_in_reg(get_n_reg(cr, pr, 3), res);
+  return (size_arg(cr, pr))
+}
+
+int exec_st(void *core, void *pro)
+{
+  t_process *pr;
+  t_core *cr;
+  unsigned int res;
 }
 
 int exec_add(void *core, void *pro)
