3af663b2cd660f6152db03cc35969caaf0222706
Allan BARRIELLE <abarriel@e2r2p6.42.fr>
2017-03-30 18:54:07 +0200

    ok

--- vm/srcs/run/get_n_arg.c
+++ vm/srcs/run/get_n_arg.c
@@ -29,37 +29,26 @@ unsigned int return_arg(t_core *core, t_process *process, int mod, int index, in
 
   // ft_printf("{%d}{%02b}\n",index, cde);
   // ft_printf("{9} [%02x] - %d\n",core->mem[(index) % MEM_SIZE], index);
- if ((cde & REG_CODE) == cde)
+ if ((cde & REG_CODE) == REG_CODE)
   {
-    // res = index;
-  // ft_printf("{9}{%p}{%d}",process->reg[index - 1],index);
-    res = 0;
+     res = 0;
     res |= core->mem[index % MEM_SIZE];
     res = chatoi(process->reg[res - 1]);
-  // ft_printf("{9}{%p}",process->reg[index - 1]);
-    // ft_printf("vache\n");
-    return(res);
-  // ft_printf("res : %08x\n", res);
+     return(res);
   }
-if ((cde & IND_CODE) == cde)
+if ((cde & IND_CODE) ==  IND_CODE)
   {
     res = chatohi(&(core->mem[index & MEM_SIZE]));
-    if(mod == 1)
+    if (mod == 1)
       res %= IDX_MOD;
     res += process->pc;
   }
-  if ((cde & DIR_CODE) == cde)
+  if ((cde & DIR_CODE) == DIR_CODE)
   {
-    //      exit(0);
      if(!process->op->l_size)
-     {
        res = chatoi(&(core->mem[index % MEM_SIZE])); 
-    // ft_printf("{%d}{%02x}",res, core->mem[index % MEM_SIZE]);
-     }
-    else
-    {
-     res = chatohi(&(core->mem[index % MEM_SIZE]));
-    }
+     else
+      res = chatohi(&(core->mem[index % MEM_SIZE]));
   }
   return(res);
 }
@@ -69,15 +58,12 @@ unsigned char return_good_value(unsigned char cde, t_process *process)
   if ((cde & REG_CODE) == REG_CODE)
     return(1);
   if ((cde & IND_CODE) == IND_CODE)
-   // { ft_printf("%d - %d",cde, IND_CODE);
     return(2);
-  
   if ((cde & DIR_CODE) == DIR_CODE)
   {
      if(!process->op->l_size)
        return(4);
-    else
-      return(2);
+    return(2);
   }
   return (0);
 }
@@ -93,30 +79,21 @@ unsigned int get_n_arg(t_core *core, t_process *process, int arg, int mod)
   cde = apply_mask(cde, arg);
   if (arg == 1)
   {
-    index =  process->pc + 2;
-    // if(index > )
-    // ft_printf("{8}{%02x}",core->mem[(process->pc + 2) % MEM_SIZE]);
-    // exit(0);
+    index = process->pc + 2;
     return(return_arg(core, process, mod, index, cde));
   }
   if (arg == 2)
   {
     i = return_good_value((((core->mem[(process->pc + 1) % MEM_SIZE]) & 192)>> 6), process);
-    // if(!process->op->l_size)
-    //     i += 2;
-    // ft_printf("{9}%d = %08b",i, cde);
     index = (process->pc + 2) + i;
-    // ft_printf("{9} [%02x] - %d\n",core->mem[(index) % MEM_SIZE], index);
-    // exit(0);
-    return(return_arg(core, process, mod, index, cde));
+      return(return_arg(core, process, mod, index, cde));
   }
   if (arg == 3)
   {
     i = return_good_value((((core->mem[(process->pc + 1) % MEM_SIZE]) & 192)>> 6), process);
     i += return_good_value((((core->mem[(process->pc + 1) % MEM_SIZE]) & 48)>> 4), process);
     index = (process->pc + 2) + i;
-    // ft_printf()
-    return(return_arg(core, process, mod, index, cde));
+     return(return_arg(core, process, mod, index, cde));
   }
   return (0);
 }
