f86712b5ac3ff01a1ba834f175d512fdb94b0e1b
Allan BARRIELLE <abarriel@e2r2p6.42.fr>
2017-03-30 18:52:20 +0200

    clean check

--- vm/srcs/run/checker.c
+++ vm/srcs/run/checker.c
@@ -16,50 +16,27 @@
 int check_all_arg(t_core *core, t_process *pro, int index, int *ok)
 {
 
-  // decal = 0;
     // ft_printf("{8}[%d] - [%b]   ",*ok,index);
      // ft_printf("{4}[%02x]\n",core->mem[(pro->pc + 1  + *ok) % MEM_SIZE]);
   if ((index & REG_CODE) == REG_CODE)
   {
-    // ft_printf("{8}[%02x]",core->mem[(pro->pc + 1 + *ok) % MEM_SIZE]);
-    // ft_printf("{8}[%08b]\n",index);
     if (core->mem[(pro->pc + 1 + *ok) % MEM_SIZE] > REG_NUMBER)
     {
       // ft_printf("REG NUMBER TROP GRAND");
       return(0);
     }
     (*ok)++;
-    // else
-    //   (*ok)++;
   }
   else if ((index & DIR_CODE) == DIR_CODE)
   {
-    // ft_printf("{8}[%02x]",core->mem[(pro->pc + 1 + i) % MEM_SIZE]);
-    // ft_printf("{8}[%08b]\n",index);
-    // ft_printf("{RED}[%02x]\n",core->mem[(pro->pc + 1 + i + *ok) % MEM_SIZE]);
     if(!pro->op->l_size)
-    {
-      // ft_printf("{8} 4[%08b] - ",index);
-        // ft_printf("{RED}4");
-      (*ok) += 4;
-      // ft_printf("{RED}[%02x]\n",core->mem[(pro->pc + 1 + i + *ok) % MEM_SIZE]);
-    }
-    else
-    {
-        // ft_printf("{RED}2");
-      (*ok) += 2;
-      // exit(0);
-    }
-      // ft_printf("T_DIR");
+          (*ok) += 4;
+       else
+     (*ok) += 2;
   }
   else if ((index & IND_CODE) == IND_CODE)
-  {
-    // ft_printf("{8}[%02x]",core->mem[(pro->pc + 1 + i) % MEM_SIZE]);
-    // ft_printf("{8}[%08b]\n",index);
-    // ft_printf("{8}[%08b]",index);
     (*ok) += 2;
-  }
-    // exit(0);
+
   return (1);
 }
 
@@ -75,10 +52,8 @@ int check_cde_oct(t_core *core, t_process *pro)
   arg = 0;
   ok = 1;
   index = core->mem[(pro->pc + 1) % MEM_SIZE];
-  // ft_printf("{RED}\n[%02x]\n",index);
   while(arg != pro->op->nbr_args)
   {
-    // ft_printf("{9}[%d]",arg);
     cde = apply_mask(index, arg + 1);
     if(cde & pro->op->type[arg])
     {
@@ -101,19 +76,9 @@ int checker_arg(t_core *core, t_process *pro)
   // ft_printf("{8}%02x- ",core->mem[(pro->pc) % MEM_SIZE]);
   if (pro->op->cde_oct == 1)
   {
-    // ft_printf("{9}%s - ",pro->op->mnemonique);
     if(!(check_cde_oct(core, pro)))
-    {
-      // exit(0);
      return(0);
-    }
    pc += 1;
-		// core->mem[(pro->pc + 2) % MEM_SIZE]
  }
-	// if(!(check_arg(core, pro, pc)))
-	// 	return(0);
-  // ft_printf("{5}%d",pro->op->type[MAX_ARGS_NUMBER]);
-  // exit(0);
-	// return (0);
  return (1);
 }
